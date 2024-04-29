ef3e1ddcd998abf384a94aaf39cab0a02d9e3bd9
ZiganshinIB <elzig3012@gmail.com>
2024-04-29 21:46:47 +0300

    get with params

--- api_dvmn/devman.py
+++ api_dvmn/devman.py
@@ -5,9 +5,9 @@ class Devman:
     def __init__(self, token):
         self.token = token
 
-    def get_request(self, url):
+    def get_request(self, url, params=None):
         """Получение данных из API"""
-        response = requests.get(url, headers={'Authorization': f'Token {self.token}'})
+        response = requests.get(url, headers={'Authorization': f'Token {self.token}'}, params=params)
         response.raise_for_status()
         raw_data = response.json()
         if 'error' in raw_data:
@@ -19,8 +19,14 @@ class Devman:
         url = 'https://dvmn.org/api/user_reviews/'
         return self.get_request(url)
 
-    def get_long_polling(self):
+    def get_long_polling(self, timestamp=None):
         """Получение данных для Long Polling"""
         url = 'https://dvmn.org/api/long_polling/'
+        if timestamp is None:
+            params = {'timestamp': str(timestamp)}
+            return self.get_request(url, params)
         return self.get_request(url)
+
+
+
 
