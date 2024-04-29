f3497098f4d32d8d350796071c719507e1075bf2
ZiganshinIB <elzig3012@gmail.com>
2024-04-29 20:27:35 +0300

    Catch a ConnectionError exception

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from environs import Env
+import time
+
 import json
+
+
+from environs import Env
+from requests import exceptions
 
 from api_dvmn import Devman
 
@@ -9,5 +14,15 @@ env.read_env()
 if __name__ == '__main__':
     dvmn = Devman(env.str('DEVMAN_TOKEN'))
     while True:
-        print(json.dumps(dvmn.get_reviews(), indent=4, ensure_ascii=False))
-        print(json.dumps(dvmn.get_long_polling(), indent=4, ensure_ascii=False))
+        try:
+            print(json.dumps(dvmn.get_reviews(), indent=4, ensure_ascii=False))
+            print(json.dumps(dvmn.get_long_polling(), indent=4, ensure_ascii=False))
+            time.sleep(5)
+        except exceptions.ReadTimeout as e:
+            print(e)
+        except exceptions.ConnectionError as e:
+            print(e)
+            print('Ошибка соединения. Повторная попытка через 5 секунд')
+            time.sleep(5)
+
+
