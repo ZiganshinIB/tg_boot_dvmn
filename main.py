59edeaa0b1ec7a12580f96a4ab080295f583160f
ZiganshinIB <elzig3012@gmail.com>
2024-04-30 07:39:00 +0300

    custom messages

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ if __name__ == '__main__':
             raw_data = json.loads(json.dumps(dvmn.get_long_polling(timestamp), indent=4, ensure_ascii=False))
             print(raw_data)
             if raw_data['status'] == 'found':
+                for attempt in raw_data['new_attempts']:
+                    text = f'У вас проверили работу [«{attempt["lesson_title"]}»]({attempt["lesson_url"]})\n'
+                    if attempt['is_negative']:
+                        text += 'К сожалению, в работе есть ошибки.'
+                    else:
+                        text += 'Преподавателю все понравилось, можно ладить дальше.'
+                    bot.send_message(text=text, chat_id=env.str('TELEGRAM_CHAT_ID'), parse_mode='Markdown')
                 bot.send_message(text='Преподаватель проверил работу!', chat_id=env.str('TELEGRAM_CHAT_ID'))
             if "timestamp_to_request" in raw_data:
                 timestamp = raw_data['timestamp_to_request']
