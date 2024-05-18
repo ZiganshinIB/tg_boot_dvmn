bf996f276b9cd912f966cc03927f78fc64d7e23b
root <root@2980245-cw17540.twc1.net>
2024-05-18 09:04:41 +0300

    Revert "refract"
    This reverts commit 28a7f350f101f7f3741b1abe01af9156d68ffe06.

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import telegram
 import logging
 
 
-def get_reviews(token, timestamp=None):
-    response = requests.get(url='https://dvmn.org/api/long_polling/', headers={'Authorization': f'Token {token}'},
-                            params={'timestamp': str(timestamp)})
+def get_long_polling_data(token, timestamp=None):
+    kwargs = {'url': 'https://dvmn.org/api/long_polling/',
+              'headers': {'Authorization': f'Token {token}'}}
+    if timestamp is None:
+        kwargs['params'] = {'timestamp': str(timestamp)}
+    response = requests.get(**kwargs)
     response.raise_for_status()
-    reviews = response.json()
-    return reviews
+    response_long_polling = response.json()
+    return response_long_polling
 
 
 if __name__ == '__main__':
@@ -20,17 +23,17 @@ if __name__ == '__main__':
     timestamp = None
     while True:
         try:
-            reviews = get_reviews(env.str('DEVMAN_TOKEN'), timestamp)
-            if reviews['status'] == 'found':
-                for attempt in reviews['new_attempts']:
+            data_long_polling = get_long_polling_data(env.str('DEVMAN_TOKEN'), timestamp)
+            if data_long_polling['status'] == 'found':
+                for attempt in data_long_polling['new_attempts']:
                     text = f'У вас проверили работу [«{attempt["lesson_title"]}»]({attempt["lesson_url"]})\n'
                     text += 'К сожалению, в работе есть ошибки.' if attempt['is_negative'] \
                         else 'Преподавателю все понравилось, можно ладить дальше.'
                     bot.send_message(text=text, chat_id=env.str('TELEGRAM_CHAT_ID'), parse_mode='Markdown')
-            if "timestamp_to_request" in reviews:
-                timestamp = reviews['timestamp_to_request']
+            if "timestamp_to_request" in data_long_polling:
+                timestamp = data_long_polling['timestamp_to_request']
             else:
-                timestamp = reviews['last_attempt_timestamp']
+                timestamp = data_long_polling['last_attempt_timestamp']
         except requests.exceptions.ReadTimeout as e:
             logging.error("Timeout error: %s", e)
         except requests.exceptions.ConnectionError as e:
