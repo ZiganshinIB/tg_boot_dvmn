e3d6136d91882f9f63872a047a88e15aa523c481
ZiganshinIB <elzig3012@gmail.com>
2024-05-19 12:31:22 +0300

    catch errors and write to log

--- logger.py
+++ logger.py
@@ -1,8 +1,12 @@
 import logging
 from settings import BOT, ADMIN_CHAT_ID
 
+logging.basicConfig(filename='log.log', filemode='w', level=logging.ERROR)
 
 class MyLogsHandler(logging.Handler):
     def emit(self, record):
         log_entry = self.format(record)
-        BOT.send_message(text=log_entry, chat_id=ADMIN_CHAT_ID,)
+        try:
+            BOT.send_message(text=log_entry, chat_id=ADMIN_CHAT_ID,)
+        except Exception as e:
+            logging.error(e)
